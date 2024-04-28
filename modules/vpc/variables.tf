c7ed86fd7c07b6366e227eadecf07382c4d03685
vishal.vishwakarma <vishal.vishwakarma@bjss.com>
2024-04-28 22:20:47 +0100

    creating public and private subnet

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -9,3 +9,18 @@ variable project_name {
 variable "vpc_cidr" {
   
 }
+
+variable "pub_sub_1a_cidr" {
+  
+}
+
+variable "pub_sub_2b_cidr" {
+  
+}
+variable "pri_sub_3a_cidr" {
+  
+}
+
+variable "pri_sub_4b_cidr" {
+  
+}
